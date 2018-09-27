44ee416ae20b148f80cb55015527c4be702c343d
Noah <noahjdu@hotmail.com>
2018-09-27 12:59:56 -0400

    Part 1 and 3 and 4 done:

---
+++ HelloWorldPluginUnity/Assets/Manipulator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manipulator : MonoBehaviour {
+    public Movement MS;
+	
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("p"))
+        {
+            commandtoMirror();
+        }
+	}
+    void commandtoMirror()
+    {
+        Debug.Log("You are now using inverted controls MWAHAHAHA");
+        MS.mirrorcontrols();
+
+    }
+
+
+}
